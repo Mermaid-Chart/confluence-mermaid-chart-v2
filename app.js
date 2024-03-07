16849cb14722c4fb73699128ebe0b5983ef8ceb6
Knut Sveidqvist <knsv@sveido.com>
2024-03-07 14:24:34 +0100

    Made removed code that is not working on vercel

--- app.js
+++ app.js
@@ -2,34 +2,34 @@
 
 // Express is the underlying that atlassian-connect-express uses:
 // https://expressjs.com
-import express from 'express';
+import express from "express";
 
 // https://expressjs.com/en/guide/using-middleware.html
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import errorHandler from 'errorhandler';
-import morgan from 'morgan';
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import errorHandler from "errorhandler";
+import morgan from "morgan";
 
 // atlassian-connect-express also provides a middleware
-import ace from 'atlassian-connect-express';
+import ace from "atlassian-connect-express";
 
 // Use Handlebars as view engine:
 // https://npmjs.org/package/express-hbs
 // http://handlebarsjs.com
-import hbs from 'express-hbs';
+import hbs from "express-hbs";
 
 // We also need a few stock Node modules
-import http from 'http';
-import path from 'path';
-import os from 'os';
-import helmet from 'helmet';
-import nocache from 'nocache';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import http from "http";
+import path from "path";
+import os from "os";
+import helmet from "helmet";
+import nocache from "nocache";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 // Routes live here; this is the C in MVC
-import routes from './routes';
+import routes from "./routes/index.js";
 
 // Bootstrap Express and atlassian-connect-express
 const app = express();
@@ -37,38 +37,42 @@ const addon = ace(app);
 
 // See config.json
 const port = addon.config.port();
-app.set('port', port);
+app.set("port", port);
 
 // Log requests, using an appropriate formatter by env
-const devEnv = app.get('env') === 'development';
-app.use(morgan(devEnv ? 'dev' : 'combined'));
+const devEnv = app.get("env") === "development";
+app.use(morgan(devEnv ? "dev" : "combined"));
 
 // We don't want to log JWT tokens, for security reasons
-morgan.token('url', redactJwtTokens);
+morgan.token("url", redactJwtTokens);
 
 // Configure Handlebars
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const viewsDir = path.join(__dirname, 'views');
-const handlebarsEngine = hbs.express4({partialsDir: viewsDir});
-app.engine('hbs', handlebarsEngine);
-app.set('view engine', 'hbs');
-app.set('views', viewsDir);
+const viewsDir = path.join(__dirname, "views");
+const handlebarsEngine = hbs.express4({ partialsDir: viewsDir });
+app.engine("hbs", handlebarsEngine);
+app.set("view engine", "hbs");
+app.set("views", viewsDir);
 
 // Atlassian security policy requirements
 // http://go.atlassian.com/security-requirements-for-cloud-apps
 // HSTS must be enabled with a minimum age of at least one year
-app.use(helmet.hsts({
-  maxAge: 31536000,
-  includeSubDomains: false
-}));
-app.use(helmet.referrerPolicy({
-  policy: ['origin']
-}));
+app.use(
+  helmet.hsts({
+    maxAge: 31536000,
+    includeSubDomains: false,
+  })
+);
+app.use(
+  helmet.referrerPolicy({
+    policy: ["origin"],
+  })
+);
 
 // Include request parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Gzip responses when appropriate
@@ -78,7 +82,7 @@ app.use(compression());
 app.use(addon.middleware());
 
 // Mount the static files directory
-const staticDir = path.join(__dirname, 'public');
+const staticDir = path.join(__dirname, "public");
 app.use(express.static(staticDir));
 
 // Atlassian security policy requirements
@@ -93,19 +97,19 @@ routes(app, addon);
 
 // Boot the HTTP server
 http.createServer(app).listen(port, () => {
-  console.log('App server running at http://' + os.hostname() + ':' + port);
+  console.log("App server running at http://" + os.hostname() + ":" + port);
 
   // Enables auto registration/de-registration of app into a host in dev mode
   if (devEnv) addon.register();
 });
 
 function redactJwtTokens(req) {
-  const url = req.originalUrl || req.url || '';
+  const url = req.originalUrl || req.url || "";
   const params = new URLSearchParams(url);
   let redacted = url;
   params.forEach((value, key) => {
-    if (key.toLowerCase() === 'jwt') {
-      redacted = redacted.replace(value, 'redacted');
+    if (key.toLowerCase() === "jwt") {
+      redacted = redacted.replace(value, "redacted");
     }
   });
   return redacted;
