32961c7eebfe252a2d396618e18b0c427ba98860
Per Brolin <per@mermaidchart.com>
2024-03-20 11:52:35 +0100

    Added logging

--- public/js/plugin.js
+++ public/js/plugin.js
@@ -1,25 +1,26 @@
-'use strict'
+"use strict";
 class CPlugin {
-  _sendMessage (data) {
-    window.parent.postMessage(data, '*')
+  _sendMessage(data) {
+    window.parent.postMessage(data, "*");
   }
   saveData(data) {
+    console.log("Incoming, save data:", data);
     if (!data.documentID || !data.diagramCode) {
       throw new Error("Invalid saving diagram data");
     }
     this._sendMessage({
       action: "save",
-      data
-    })
+      data,
+    });
   }
   cancel() {
     this._sendMessage({
-      action: "cancel"
-    })
+      action: "cancel",
+    });
   }
   getData() {
-    return window.name ? JSON.parse(window.name) : {}
+    return window.name ? JSON.parse(window.name) : {};
   }
 }
 
-window.CP = new CPlugin()
+window.CP = new CPlugin();
